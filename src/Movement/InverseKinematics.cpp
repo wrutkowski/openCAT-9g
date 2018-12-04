8e2a76d52da49ef58d3862d1055a7674f895cdad
Wojciech Rutkowski <me@wojciech-rutkowski.com>
2018-12-04 23:17:50 +0000

    Mathematical model for Inverse Kinematics (not tested on a robot)

--- src/Movement/InverseKinematics.cpp
+++ src/Movement/InverseKinematics.cpp
@@ -1,10 +1,19 @@
 #include "InverseKinematics.h"
 
-InverseKinematicsModel InverseKinematics::model(LEG leg, float x, float y, float z, float yaw, float pitch, float roll) {
+InverseKinematicsModel InverseKinematics::model(LEG leg, float x, float y, float z_, float yaw, float pitch, float roll) {
 
     InverseKinematicsModel model;
 
-    // TODO magic
+    float z = -z_;
+    float L = sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2));
+
+    model.knee = acos((pow(femurLength, 2) + pow(tibiaLength, 2) - pow(L, 2)) / (2 * femurLength * tibiaLength));
+
+    float knee1 = acos((pow(L, 2) + pow(femurLength, 2) - pow(tibiaLength, 2)) / (2 * L * femurLength));
+    float knee2 = acos((pow(L, 2) + pow(z, 2) - pow(x, 2)) / (2 * L * z) );
+    model.shoulderPivot = knee1 + knee2;
+
+    model.shoulderPivotSides = atan2(y, L);
 
     return model;
 }
